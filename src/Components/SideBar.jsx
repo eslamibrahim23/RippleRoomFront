46abd846f25997ea66ae1eafc3943c23b13b5e3e
Eslam Ibrahim <134802239+eslamibrahim23@users.noreply.github.com>
2024-03-14 16:30:30 +0200

    b

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import {AiOutlineMessage} from 'react-icons/ai'
 import { FaHome,FaCog,FaBell,FaSignOutAlt  } from 'react-icons/fa';
 
@@ -14,6 +14,7 @@ function SideBar() {
             <li className='mb-5'><a href=""><FaCog className='text-[1.3rem]'/><span className='pl-2'>Settings</span></a></li>
             <li className='mt-44'><a href='/login'><FaSignOutAlt className='text-[1.3rem]'/><span className='pl-2'>SignOut</span></a> </li>
         </ul>
+     
     </div>
     </div>
   )
