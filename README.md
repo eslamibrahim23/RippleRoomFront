01e19b9b0ff14054d840b9fae3237b3a0babce4b
Eslam Ibrahim <134802239+eslamibrahim23@users.noreply.github.com>
2024-03-16 12:21:59 +0200

    changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+Hello From Zag
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {AiOutlineMessage} from 'react-icons/ai'
+import { FaHome,FaCog,FaBell,FaSignOutAlt  } from 'react-icons/fa';
+
+function SideBar() {
+  return (
+    <div className={`sidebar-container`}>
+        <h1 className={`logo`}>RippleRoom</h1>
+        <div>
+        <ul className='sidebar last:absolute bottom-10'>
+            <li className='mb-5'><a href=""><FaHome className='text-[1.3rem]'/><span className='pl-2'>Home</span></a></li>
+            <li className='mb-5'><a href=""><AiOutlineMessage className='text-[1.3rem]'/><span className='pl-2'>Messages</span></a></li>
+            <li className='mb-5'><a href=""><FaBell className='text-[1.3rem]'/><span className='pl-2'>Notifications</span></a></li>
+            <li className='mb-5'><a href=""><FaCog className='text-[1.3rem]'/><span className='pl-2'>Settings</span></a></li>
+            <li className='mt-44'><a href='/login'><FaSignOutAlt className='text-[1.3rem]'/><span className='pl-2'>SignOut</span></a> </li>
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default SideBar
