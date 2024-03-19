1e2c5ba80641a4b9f1a4946489d2f84a8908116c
Eslam Ibrahim <134802239+eslamibrahim23@users.noreply.github.com>
2024-03-19 14:30:26 +0200

    adding edit profile

--- src/Components/main/edituser/EditProfile.tsx
+++ src/Components/main/edituser/EditProfile.tsx
@@ -2,36 +2,52 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import profile from "../../../assets/images/R.png";
+import profile from "../../../assets/images/avatar.jpg";
 import { Link } from "react-router-dom";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "../../ui/input";
+import { cn } from "@/utils/cn";
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <div className={cn(" w-[25.2rem]", className)}>{children}</div>;
+};
 
 const EditProfile: React.FC = () => {
   const { register, handleSubmit, setValue } = useForm();
   const [avatar, setAvatar] = useState<File | null>(null);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const userId = localStorage.getItem("userId");
+
   const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setAvatar(e.target.files[0]);
-      // setImageUrl(URL.createObjectURL(e.target.files[0]));
+      setImageUrl(URL.createObjectURL(e.target.files[0]).value);
     }
   };
-  axios
-    .get(`https://rippleroomback.onrender.com/user/profile/${userId}`)
-    .then((response) => {
-      const userData = response.data;
-      Object.keys(userData).forEach((key) => {
-        setValue(key, userData[key]);
-      });
+  useEffect(() => {
+    axios
+      .get(`https://rippleroomback.onrender.com/user/profile/${userId}`)
+      .then((response) => {
+        const userData = response.data;
+        Object.keys(userData).forEach((key) => {
+          setValue(key, userData[key]);
+        });
 
-      if (userData.Image) {
-        setImageUrl(userData.Image); // Set image data in state
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching user profile:", error);
-    });
-  useEffect(() => {}, [setValue]);
+        if (userData.Image) {
+          setImageUrl(userData.Image); // Set image data in state
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user profile:", error);
+      });
+  }, [setValue]);
+
+  const userId = localStorage.getItem("userId");
 
   const onSubmit = (data: any) => {
     const formDataToSend = new FormData();
@@ -41,7 +57,6 @@ const EditProfile: React.FC = () => {
     formDataToSend.append("Address", data.Address);
     formDataToSend.append("Password", data.Password);
     formDataToSend.append("Bio", data.Bio);
-    formDataToSend.append("Image", data.Image);
 
     // Only append the Image field once
     if (avatar) {
@@ -52,7 +67,7 @@ const EditProfile: React.FC = () => {
 
     axios
       .patch(
-       ` https://rippleroomback.onrender.com/user/editprofile/${userId}`,
+        `https://rippleroomback.onrender.com/user/editprofile/${userId}`,
         formDataToSend
       )
       .then((response) => {
@@ -65,143 +80,86 @@ const EditProfile: React.FC = () => {
     console.log(data);
   };
 
-  return (
+  return(
     <>
-      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Edit Profile
-        </h2>
-        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
-          {/* Image */}
-          <img
-            src={
-              imageUrl
-                ? `https://rippleroomback.onrender.com/${imageUrl}`
-                : profile
-            }
-            {...register("Image")}
-            alt="Profile"
-            className="w-40 h-40 rounded-full cursor-pointer mb-4"
-            onClick={() => document.getElementById("imageInput").click()}
-          />
-          {/* File input */}
-          <input
-            type="file"
-            id="imageInput"
-            style={{ display: "none" }}
-            onChange={(e) => handleAvatarChange(e)}
-          />
-          {/* Username and Bio */}
-          <div className="flex mb-4">
-            <div className="w-1/2 mr-2">
-              <label
-                htmlFor="userName"
-                className="block text-neutral-700 dark:text-neutral-300"
-              >
-                Username
-              </label>
-              <input
-                {...register("userName")}
-                id="userName"
-                className="input"
-                placeholder="Username"
-              />
-            </div>
-            <div className="w-1/2 ml-2">
-              <label
-                htmlFor="bio"
-                className="block text-neutral-700 dark:text-neutral-300"
-              >
-                Bio
-              </label>
-              <input
-                {...register("Bio")}
-                id="bio"
-                className="input"
-                placeholder="Bio"
-              />
-            </div>
-          </div>
-          {/* Email and Phone */}
-          <div className="flex mb-4">
-            <div className="w-1/2 mr-2">
-              <label
-                htmlFor="email"
-                className="block text-neutral-700 dark:text-neutral-300"
-              >
-                Email
-              </label>
-              <input
-                {...register("Email")}
-                id="email"
-                className="input"
-                placeholder="Email"
-              />
-            </div>
-            <div className="w-1/2 ml-2">
-              <label
-                htmlFor="phone"
-                className="block text-neutral-700 dark:text-neutral-300"
-              >
-                Phone
-              </label>
-              <input
-                {...register("Phone")}
-                id="phone"
-                className="input"
-                placeholder="Phone"
-              />
-            </div>
-          </div>
-          {/* Address */}
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="block text-neutral-700 dark:text-neutral-300"
-            >
-              Address
-            </label>
-            <input
-              {...register("Address")}
-              id="address"
-              className="input"
-              placeholder="Address"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-neutral-700 dark:text-neutral-300"
-            >
-              Password
-            </label>
-            <input
-              {...register("Password")}
-              id="password"
-              className="input"
-              placeholder="Password"
-              type="password"
-            />
-          </div>
-          {/* Save and Cancel buttons */}
-          <div className="flex justify-between items-center">
-            <Link
-              to="/home"
-              className="px-4 py-2 rounded border border-solid border-purple-500"
-            >
-              Cancel
-            </Link>
-            <button
-              type="submit"
-              className="bg-purple-700 text-white px-6 py-2 rounded"
-            >
-              Save
-            </button>
-          </div>
-        </form>
+    <form className="grid grid-cols-1 md:grid-cols-2 gap-5 items-end w-[90%] mx-auto" onSubmit={handleSubmit(onSubmit)}>
+    <div className="text-center col-span-1 mx-10 md:block">
+        <div className="image mx-auto">
+        <img
+             src={
+               imageUrl
+                 ? `https://rippleroomback.onrender.com/${imageUrl}`
+                 : profile
+             }
+             alt="Profile"
+             className="w-72 h-72 rounded-full cursor-pointer"
+             onClick={() => document.getElementById("imageInput").click()}
+           />
+           {/* File input */}
+           <input
+             type="file"
+             id="imageInput"
+             style={{ display: "none" }}
+             onChange={(e) => handleAvatarChange(e)}
+           />
+        </div>
       </div>
-    </>
-  );
+      <div className="col-span-1 ">
+       
+        <LabelInputContainer>
+        <Label htmlFor="email">Email</Label>
+          <Input type="text" id="email" {...register("Email")} className="py-6" placeholder="Email"/>
+          </LabelInputContainer>
+        
+      </div>
+      <div className="col-span-1">
+        <LabelInputContainer>
+        <Label htmlFor="yourname">Your Name</Label>
+          <Input type="text" id="yourname" {...register("userName")} className="py-6" placeholder="userName"/>
+          </LabelInputContainer>
+      </div>
+      
+      
+      <div className="col-span-1">
+        <LabelInputContainer>
+        <Label htmlFor="phone">Phone</Label>
+          <Input type="text" id="phone" {...register("Phone")} className="py-6" placeholder="Phone"/>
+          </LabelInputContainer>
+        
+      </div>
+      <div className="col-span-1">
+        <LabelInputContainer>
+        <Label htmlFor="bio">Bio</Label>
+          <Input type="text" id="bio" {...register("Bio")} className="py-6" placeholder="Bio"/>
+          </LabelInputContainer>
+      </div>
+      <div className="col-span-1">
+       
+        <LabelInputContainer>
+        <Label htmlFor="address">Address</Label>
+          <Input type="text" id="address" {...register("Address")} className="py-6" placeholder="Address"/>
+          </LabelInputContainer>
+        
+      </div>
+      <div className="flex justify-between items-center mt-10 mx-44">
+          <Link
+            to="/ChatPage"
+            className="px-8 py-2 mx-32 rounded border border-solid border-purple-500"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="bg-purple-700 text-white px-10 py-2 rounded"
+          >
+            Save
+          </button>
+        </div>
+        </form>
+  </>
+  )
+
 };
+
 
 export default EditProfile;
--- src/components/main/SignupForm.tsx
+++ src/components/main/SignupForm.tsx
@@ -5,8 +5,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 // import { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-import { useNavigate , Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { cn } from "@/utils/cn";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 ("use client");
@@ -15,8 +17,6 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../googleSign/firebaseConfig";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import Logo from "./Logo";
 // import { useEffect, useState } from "react";
 // import { log } from "console";
@@ -30,12 +30,9 @@ const LabelInputContainer = ({
   className?: string;
 }) => {
   return (
-    <>
-      <Logo />
-      <div className={cn("flex flex-col space-y-2 w-full", className)}>
-        {children}
-      </div>
-    </>
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
   );
 };
 const BottomGradient = () => {
@@ -100,7 +97,7 @@ export default function SignupForm() {
           { Email: data.user.email, userName: data.user.displayName }
         );
         console.log(fetchuser);
-        navigate("/chatpage");
+        navigate("/ChatPage");
       };
       tt();
     });
@@ -119,110 +116,113 @@ export default function SignupForm() {
 
   return (
     <>
-    <div className="flex start-10 py-8 fixed top-0 w-full ">
-          <Link to="/">
-            <span className="text-purple-500 sm:text-xl md:text-3xl lg:text-4xl font-bold text-center mr-3">
-              Ripple
-            </span>
-            <span className="text-blue-500 sm:text-lg md:text-3xl lg:text-2xl font-bold text-center">
-              Room
-            </span>
-          </Link>
-        </div> 
-      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black  ">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Welcome to Ripple Room 😀
-        </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Sign up to ripple to enjoy chatting with friends
-        </p>
+      <div className="min-h-screen flex flex-col justify-center items-center p-4 md:p-8">
+        <Logo />
 
-        <form className="w-50 my-5" onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="userName">User name</Label>
-              <Input
-                id="userName"
-                placeholder="user name"
-                type="text"
-                {...register("userName")}
-              />
-              <p className="text-red-500 text-md">{errors.userName?.message}</p>
-            </LabelInputContainer>
-          </div>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="Email">Email</Label>
-              <Input
-                id="Email"
-                placeholder="Email"
-                type="text"
-                {...register("Email")}
-              />
-              <p className="text-red-500 text-md">{errors.Email?.message}</p>
-            </LabelInputContainer>
-          </div>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="Password">Password</Label>
-              <Input
-                id="Password"
-                placeholder="Password"
-                type="password"
-                {...register("Password")}
-              />
-              <p className="text-red-500 text-md ">
-                {errors.Password?.message}
-              </p>
-            </LabelInputContainer>
-          </div>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="cPassword">Confirm Password</Label>
-              <Input
-                id="cPassword"
-                placeholder="user name"
-                type="password"
-                {...register("cPassword")}
-              />
-              <p className="text-red-500 text-md ">
-                {errors.cPassword?.message}
-              </p>
-            </LabelInputContainer>
-          </div>
-
-          <button
-            className="bg-gradient-to-br relative group/btn from-purple-500 dark:from-zinc-900 dark:to-zinc-900 to-neutral-400 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Sign up
-            <BottomGradient />
-          </button>
-        </form>
-
-        <div className="flex flex-row items-center space-y-2 md:space-y-0 md:space-x-2 mb-2 w-full">
-          <div className="bg-gray-200 via-neutral-300 dark:via-neutral-700 to-transparent my-6 h-[1px] w-32" />
-          <p className="text-neutral-600 text-xs max-w-sm  dark:text-neutral-300 text-center">
-            OR CONTINUE WITH
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black  ">
+          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+            Welcome to Ripple Room 😀
+          </h2>
+          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            Sign up to ripple to enjoy chatting with friends
           </p>
-          <div className="bg-gray-200 via-neutral-300 dark:via-neutral-700 to-transparent h-[1px] w-32" />
-        </div>
 
-        <div
-          className="
-        justify-center
-        items-center
-        flex bg-gradient-to-br relative group/btn from-sky-500 dark:from-zinc-900 dark:to-zinc-900 to-neutral-400 block dark:bg-zinc-800 w-full  rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-        >
-          <div className="">
-            <FontAwesomeIcon
-              icon={faGoogle}
-              style={{ fontSize: "30px", color: "rgb(168, 85, 247, 0)" }}
-            />
+          <form className="w-50 my-5" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="userName">User name</Label>
+                <Input
+                  id="userName"
+                  placeholder="user name"
+                  type="text"
+                  {...register("userName")}
+                />
+                <p className="text-red-500 text-md">
+                  {errors.userName?.message}
+                </p>
+              </LabelInputContainer>
+            </div>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="Email">Email</Label>
+                <Input
+                  id="Email"
+                  placeholder="Email"
+                  type="text"
+                  {...register("Email")}
+                />
+                <p className="text-red-500 text-md">{errors.Email?.message}</p>
+              </LabelInputContainer>
+            </div>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="Password">Password</Label>
+                <Input
+                  id="Password"
+                  placeholder="Password"
+                  type="password"
+                  {...register("Password")}
+                />
+                <p className="text-red-500 text-md ">
+                  {errors.Password?.message}
+                </p>
+              </LabelInputContainer>
+            </div>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="cPassword">Confirm Password</Label>
+                <Input
+                  id="cPassword"
+                  placeholder="user name"
+                  type="password"
+                  {...register("cPassword")}
+                />
+                <p className="text-red-500 text-md ">
+                  {errors.cPassword?.message}
+                </p>
+              </LabelInputContainer>
+            </div>
+
+            <button
+              className="bg-gradient-to-br relative group/btn from-purple-500 dark:from-zinc-900 dark:to-zinc-900 to-neutral-400 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)inset,0px-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+            >
+              Sign up
+              <BottomGradient />
+            </button>
+          </form>
+
+          <div className="flex flex-row items-center space-y-2 md:space-y-0 md:space-x-2 mb-2 w-full">
+            <div className="bg-gray-200 via-neutral-300 dark:via-neutral-700 to-transparent my-6 h-[1px] w-32" />
+            <p className="text-neutral-600 text-xs max-w-sm  dark:text-neutral-300 text-center">
+              OR CONTINUE WITH
+            </p>
+            <div className="bg-gray-200 via-neutral-300 dark:via-neutral-700 to-transparent h-[1px] w-32" />
           </div>
-          <button onClick={handleclick} className="text-2xl" type="submit">
-            oogle
-          </button>
+
+          <div
+            className="
+  justify-center
+  items-center
+  flex bg-gradient-to-br relative group/btn white-500 dark:from-zinc-900 dark:to-zinc-900 to-neutral-400 block dark:bg-zinc-800 w-full  rounded-md h-10 font-medium 
+  shadow-md text-blue-500 dark:text-blue-500
+  "
+          >
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className="text-purple-500"
+                icon={faGoogle}
+                style={{ fontSize: "35px" }}
+              />
+              <button
+                onClick={handleclick}
+                className="text-2xl text-blue-500 ml-2"
+                type="submit"
+              >
+                oogle
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
