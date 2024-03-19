a7003ef8408e1bb290d99bb127c4200bad3acea6
Mahmoudy71 <158741403+Mahmoudy71@users.noreply.github.com>
2024-03-19 17:21:11 +0200

    Shawya responsive Kelapy Keda

--- src/Components/main/TypewriterEffectDemo.tsx
+++ src/Components/main/TypewriterEffectDemo.tsx
@@ -13,34 +13,37 @@ export function TypewriterEffectDemo() {
     {
       text: "To",
     },
-    {
-      text: "Ripple",
-      className: "text-purple-600 dark:text-purple-600",
-    },
-    {
-      text: "Room.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
+     {
+    text: "Ripple",
+    className: "text-purple-600 dark:text-purple-600",
+  },
+  {
+    text: "Room.",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
+
+
   return (
     <>
-      {/* <Drawer /> */}
-      <Link to="/login" style={{ cursor: "pointer" }}>
+     
+     {/* <Drawer /> */}
+     <Link to="/login" style={{ cursor: "pointer" }}>
         <Logo />
       </Link>
-      <div className="flex flex-col items-center justify-center h-full ">
-        <p className="text-neutral-900 dark:text-neutral-200 text-base  mb-10">
+      <div className="flex flex-col items-center justify-center h-full px-4">
+        <p className="text-neutral-900 dark:text-neutral-200 text-base md:text-lg lg:text-xl xl:text-2xl text-center mb-10">
           You must be the change you wish to see in the world.
         </p>
         <TypewriterEffect words={words} />
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-10">
           <Link to={"/login"}>
-            <button className="w-40 h-10 rounded-xl bg-blue-500 border dark:border-white border-transparent text-white text-sm">
+            <button className="w-full md:w-40 h-10 rounded-xl bg-blue-500 border dark:border-white border-transparent text-white text-sm md:text-base">
               Login
             </button>
           </Link>
           <Link to={"/signup"}>
-            <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+            <button className="w-full md:w-40 h-10 rounded-xl bg-white text-black border border-black text-sm md:text-base md:px-2">
               Signup
             </button>
           </Link>
--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -8,24 +8,24 @@ function Home() {
     <div>
 
 
-    <div className='text-center	space-y-5 '>
-        <div>
-          <span className="font-bold text-3xl">Welcome To</span>
-          <span className="text-purple-500 font-bold text-3xl mx-1">
-            Ripple
-          </span>
-          <span className="text-blue-500 font-bold text-2xl">
-            Room
-          </span>
-        </div>
-        <div className="text-base">
-        Connect and chatting with friends easier and faster around the world.
-        </div>
-        <Link to="/Msg">
-      <button className="btn mt-5 border-0 w-40 font-semibold text-base bg-violet-500 text-white  hover:bg-white hover:text-purple-600">Let's Start</button>
-      </Link>
+<div className="text-center space-y-5 px-4 md:px-0">
+  <div className="md:flex md:flex-col md:items-center">
+    <div>
+      <span className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Welcome To</span> 
+      <span className="text-purple-500 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl mx-1">Ripple</span>
+      <span className="text-blue-500 font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">Room</span> 
     </div>
+    <div className="text-base">
+      Connect and chat with friends easier and faster around the world.
     </div>
+    <Link to="/Msg">
+      <button className="btn mt-5 border-0 w-40 sm:w-auto font-semibold text-base sm:text-lg bg-violet-500 text-white hover:bg-white hover:text-purple-600">Let's Start</button> 
+    </Link>
+  </div>
+  </div>
+  </div>
+
+
   )
 }
 
